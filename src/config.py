063defd3ba87db3749b6c0f6b2ea1a6d2e84f4c4
ibrvtk <nicecock@disroot.org>
2026-04-24 09:41:09 +0300

    config.py: Add comments

--- src/config.py
+++ src/config.py
@@ -7,6 +7,8 @@ from os import getenv
 
 from database import Database
 
+# Распаковка .env
+
 BOT_TOKEN=getenv('BOT_TOKEN')
 
 DB_HOST=getenv('DB_HOST')
@@ -14,6 +16,8 @@ DB_DBNAME=getenv('DB_DBNAME')
 DB_PORT=getenv('DB_PORT')
 DB_USER=getenv('DB_USER')
 DB_PASSWORD=getenv('DB_PASSWORD')
+
+# Создание объекта бота и базы данных
 
 bot = Bot(
     token=BOT_TOKEN,
@@ -28,48 +32,54 @@ bot = Bot(
 )
 db = Database()
 
+# Данные бота (чтобы на вызывать bot.get_me())
+
 BOT_TID = 8751189083
 BOT_USERNAME = "localwebot"
 BOT_FULL_NAME = "Setka (Localweb) — Бот для создания сеток чатов"
 
+# Иерархия модерации
+
 post_str = {
     # Преобразует технические названия должности админа в пользовательские
-    "owner": "Владелец паутины",
-    "heir": "Наследник",
-    "admin": "Администратор",
-    "adminjr": "Младший администратор",
-    "moder": "Модератор",
-    "helper": "Помощник",
-    "user": "Пользователь"
+    "owner"  : "Владелец паутины" ,
+    "heir"   : "Наследник"        ,
+    "admin"  : "Администратор"    ,
+    "adminjr": "Старший модератор",
+    "moder"  : "Модератор"        ,
+    "helper" : "Помощник"         ,
+    "user"   : "Пользователь"
 }
 post_strstr = {
     # Преобразует пользовательские названия должности админа в технические
-    post_str['owner']: "owner",
-    post_str['heir']: "heir",
-    post_str['admin']: "admin",
+    post_str['owner']  : "owner"  ,
+    post_str['heir']   : "heir"   ,
+    post_str['admin']  : "admin"  ,
     post_str['adminjr']: "adminjr",
-    post_str['moder']: "moder",
-    post_str['helper']: "helper",
-    post_str['user']: "user"
+    post_str['moder']  : "moder"  ,
+    post_str['helper'] : "helper" ,
+    post_str['user']   : "user"
 }
 post_intstr = {
     # Преобразует цифру 0-5 в должность админа (по иерархии)
-    5: "owner",
-    4: "admin",
+    5: "owner"  ,
+    4: "admin"  ,
     3: "adminjr",
-    2: "moder",
-    1: "helper",
+    2: "moder"  ,
+    1: "helper" ,
     0: "user"
 }
 post_strint = {
     # Преобразует должность админа в цифру 0-5 (по иерархии)
-    "owner": 5,
-    "admin": 4,
-    "adminjr": 3,
-    "moder": 2,
-    "helper": 1,
-    "user": 0
+    post_intstr[5]: 5,
+    post_intstr[4]: 4,
+    post_intstr[3]: 3,
+    post_intstr[2]: 2,
+    post_intstr[1]: 1,
+    post_intstr[0]: 0
 }
+
+# Пунктуация, кириллица (взято из модуля string и адаптировано)
 
 punctuation = r"""!"#$%&'()*+,-./:;<=>?@[\]^`{|}~ """
 cyrillic_lowercase = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
